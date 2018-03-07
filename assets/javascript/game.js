c276e7d394bf60ee5de1615ff40231b5f16870c0
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-07 17:50:28 -0600

    Added vars for crystal numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 var unknownNumber;
 var totalScore;
 
+var number1 = 1 + Math.floor(Math.random() * 12);
+var number2 = 1 + Math.floor(Math.random() * 12);
+var number3 = 1 + Math.floor(Math.random() * 12);
+var number4 = 1 + Math.floor(Math.random() * 12);
 
 
 
